386ff0711e41bf7360d49607257d487f37bd3ae2
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2024-06-01 18:31:27 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp -a ./html/. /var/www/html
 cp -a ./systemd/system/. /etc/systemd/system
+mkdir ~/Desktop/dms_files
 systemctl daemon-reload
 systemctl enable --now dead-mans-switch.timer
 systemctl start dead-mans-switch.service
