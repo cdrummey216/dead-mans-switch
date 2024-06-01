2ee14bf85de0084706aafbf57473e9c2086e07fd
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2024-05-31 21:43:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 cp -a ./html/. /var/www/html
 cp -a ./systemd/system/. /etc/systemd/system
+systemctl daemon-reload
+systemctl enable --now dead-mans-switch.timer
+systemctl start dead-mans-switch.service
