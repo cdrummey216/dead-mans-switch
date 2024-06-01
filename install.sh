d718f72102da369fef1f87b67a7a77228f0ed15d
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2024-06-01 18:34:19 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp -a ./html/. /var/www/html
 cp -a ./systemd/system/. /etc/systemd/system
+TZ=UTC date +%s > "/var/www/html/dms_logs/dmt.txt"
 mkdir ~/Desktop/dms_files
 systemctl daemon-reload
 systemctl enable --now dead-mans-switch.timer
