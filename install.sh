0ba5501e47e774f507d8609d638c9097866d9fd3
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2024-06-01 21:35:43 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,51 @@
 #!/usr/bin/env bash
-cp -a ./html/. /var/www/html
-cp -a ./systemd/system/. /etc/systemd/system
-TZ=UTC date +%s > "/var/www/html/dms_logs/dmt.txt"
-mkdir ~/Desktop/dms_files
-systemctl daemon-reload
-systemctl enable --now dead-mans-switch.timer
-systemctl start dead-mans-switch.service
+#
+# Copy the html files
+#
+if ! cp -a ./html/. /var/www/html; then
+    echo "Failed to copy to /var/www/html"
+fi
+#
+# Copy the service and timer
+#
+if ! cp -a ./systemd/system/. /etc/systemd/system; then
+    echo "Failed to copy to /etc/systemd/system"
+fi
+#
+# Update the timestamp to current time
+#
+if ! date +%s > "/var/www/html/dms_logs/dmt.txt"; then
+    echo "Failed to update timestamp"
+fi
+#
+# Create the desktop folder for dms files
+#
+if ! mkdir "/home/cdrummey/Desktop/dms_files"; then
+    echo "Failed to create /dms_files on the desktop, may already exist"
+fi
+#
+# Start service
+#
+if ! systemctl enable --now dead-mans-switch.service; then
+    echo "Failed to enable dead-mans-switch.service"
+    exit 1
+fi
+#
+# Start timer
+#
+if ! systemctl enable --now dead-mans-switch.timer; then
+    echo "Failed to enable dead-mans-switch.timer"
+    exit 1
+fi
+#
+# Reload daemon
+#
+if ! systemctl daemon-reload; then
+    echo "Failed to daemon-reload"
+fi
+#
+# Reset daemon
+#
+if ! systemctl reset-failed; then
+    echo "Failed to reset-failed"
+fi
