21d6bee96861a7f24623861ef4f51d06a035ebe8
deadman <46973458+cdrummey216@users.noreply.github.com>
2024-07-03 21:16:22 -0400

    Add files via upload

--- html/send-mail.sh
+++ html/send-mail.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-#runuser -l  user -c '/home/user/Downloads/dead-mans-switch-main/html/send-mail.sh example@gmail.com'
+
 datapath="/var/www/html/dms_files"
 dead_man_message_path="/var/www/html/dmm.txt"
 dead_man_email_path="/var/www/html/dme.txt"
-read -d '' -r -a dmmessage < /var/www/html/dmm.txt
+
+#read -d '' -r -a dmmessage < /var/www/html/dmm.txt
+dmmessage=$(</var/www/html/dmm.txt)
 
 arrVar=()
 search_dir=${datapath}
@@ -17,16 +19,13 @@ done
 printf -v joined '%s,' "${arrVar[@]}"
 echo "${joined%,}"
 
-dead_man_attachment_paths=(${arrVar[@]})
-
-echo $1
-sleep 1
-
-thunderbird -compose "to='$1',subject='A Dead Man's Switch was activated',body='${dmmessage}',attachment='${joined%,}'" &
-sleep 5
-ydotool key ctrl+enter
-
-sleep 5
+#echo "${dmmessage}"
+export DISPLAY=:0
+printf "%s\n Sending confirmation email..." && thunderbird -compose "to='$1',subject='A Dead Man's Switch was activated',body='$dmmessage',attachment='${joined%,}'" & 
+sleep 2
+ydotool key ctrl+enter 
+sleep 2 
 ydotool key enter
+
 echo "Done!"
 
--- html/send-mail_warning.sh
+++ html/send-mail_warning.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#runuser -l  user -c '/home/user/Downloads/dead-mans-switch-main/html/send-mail.sh cdrummey216@gmail.com'
 datapath="/var/www/html/dms_files"
 dead_man_message_path="/var/www/html/dmm.txt"
 dead_man_email_path="/var/www/html/dme.txt"
@@ -20,12 +19,12 @@ done
 printf -v joined '%s,' "${arrVar[@]}"
 echo "${joined%,}"
 
-echo "${dmmessage}"
+#echo "${dmmessage}"
 export DISPLAY=:0
 printf "%s\n Sending confirmation email..." && thunderbird -compose "to='$1',subject='A Dead Man's Switch is about to activate',body='$dmmessage',attachment='${joined%,}'" & 
-sleep 5
+sleep 2
 ydotool key ctrl+enter 
-sleep 5 
+sleep 2 
 ydotool key enter
 
 echo "Done!"
