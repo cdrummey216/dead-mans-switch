5e68ecaa8a65a4808692310e468644870d815e7e
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-09-26 18:57:24 -0400

    Add files via upload

--- html/shell.php
+++ html/shell.php
@@ -1,36 +1,10 @@
 <!DOCTYPE html>
-<?php
-    $output = "";
-    $command = "";
-    switch ($_GET['execute']) {
-        case 1:
-            $command ="/usr/bin/systemctl is-active dead-mans-switch.timer";
-            $output = shell_exec($command);
-            break;
-        case 2:
-            $command ="/usr/bin/systemctl start dead-mans-switch.service";
-            $output = shell_exec($command);
-            break;
-	case 3:
-            $command ="/usr/bin/systemctl stop dead-mans-switch.service";
-            $output = shell_exec($command);
-            break;
-	case 4:
-            $command ="/usr/bin/bash /var/www/html/dms_test.sh";
-            $output = shell_exec($command);
-            break;
-	case 5:
-            $command ="/usr/bin/bash /var/www/html/dms-update.sh";
-            $output = shell_exec($command);
-            break;
-        default: die("Missing Command");
-    }
-?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Restricted Area</title>
-    <LINK href="style.css" rel="stylesheet" type="text/css">
+    <title>Caveman's Terminal</title>
+    <LINK rel="stylesheet" href="style.css?v=<?php echo time(); ?>" type="text/css">
     <style>
         body {
             margin: 5vw;
@@ -41,8 +15,7 @@
         form {
             width: 30vw;
         }
-    </style>
-    <style>
+        
 .clock {
     color: black;
     font-size: 60px;
@@ -71,6 +44,41 @@
 
 </style>
 </head>
+<?php
+    $output = "";
+    $command = "";
+    switch ($_GET['execute']) {
+        case 1:
+            $command ="/usr/bin/systemctl is-active dead-mans-switch.timer";
+            $output = shell_exec($command);
+            break;
+        case 2:
+            $command ="/usr/bin/systemctl start dead-mans-switch.service";
+            $output = shell_exec($command);
+            break;
+	case 3:
+            $command ="/usr/bin/systemctl stop dead-mans-switch.service";
+            $output = shell_exec($command);
+            break;
+	case 4:
+            $command ="/usr/bin/bash /var/www/html/dms_test.sh";
+            $output = shell_exec($command);
+            break;
+	case 5:
+            $command ="/usr/bin/bash /var/www/html/dms-update.sh";
+            $output = shell_exec($command);
+            break;
+        case 6:
+            $command ="/usr/bin/bash /var/www/html/dms.sh";
+            $output = shell_exec($command);
+            break;
+        case 7:
+            $command ="/usr/games/fortune";
+            $output = shell_exec($command);
+            break;
+        default: die("Missing Command");
+    }
+?>
 <body>
 	<h1>Caveman's Terminal</h1>
 	
@@ -79,8 +87,9 @@
 	<a type="submit" class="button" href="?execute=3">stop</a>
     	<a type="submit" class="button" href="?execute=4">test</a>
     	<a type="submit" class="button" href="?execute=5">update</a>
-	<h2>Command Output</h4>
-	<div class="consolebody"><?php echo nl2br($output); ?></div>
-	
+    	<a type="submit" class="button" href="?execute=6">run</a>
+    	<a type="submit" class="button" href="?execute=7">fortune</a>
+	<h4>command: <?php echo nl2br($command); ?></h4>
+	<div class="consolebody">output: <?php echo nl2br($output); ?></div>
 </body>
 </html>
